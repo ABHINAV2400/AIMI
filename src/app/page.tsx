cfb110030257ceaa91fba75b93377ccf65b5c79f
ABHINAV2400 <bollaabhinav2412@gmail.com>
2025-07-29 00:20:39 +0200

    Refactor database schema and API to support new message handling.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,18 +3,20 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { useTRPC } from "@/trpc/client";
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
 import { useState } from "react";
 import { toast } from "sonner";
+import { json } from "stream/consumers";
 
 const Page = () => {
   const [value, setValue] = useState("");
 
   const trpc = useTRPC();
-  const invoke = useMutation(
-    trpc.invoke.mutationOptions({
+  const { data: messages } = useQuery(trpc.messages.getMany.queryOptions());
+  const createMessages = useMutation(
+    trpc.messages.create.mutationOptions({
       onSuccess: () => {
-        toast.success("Background job started");
+        toast.success("create");
       },
     })
   );
@@ -22,9 +24,13 @@ const Page = () => {
   return (
     <div className="p-4 max-w-7xl mx-auto">
       <Input value={value} onChange={(e) => setValue(e.target.value)} />
-      <Button disabled={invoke.isPending} onClick={() => invoke.mutate({ value: value })}>
+      <Button
+        disabled={createMessages.isPending}
+        onClick={() => createMessages.mutate({ value: value })}
+      >
         Invoke background job
       </Button>
+      {JSON.stringify(messages, null, 2)}
     </div>
   );
 };
